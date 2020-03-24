6aabdde870b8245211389d286e3018f2d41423dd
Devgen <dev@generationyct.nl>
2020-03-24 11:57:45 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const passport     = require('passport')
+const UserModel    = require('./models/user')
+
+
+mongoose
+  .connect('mongodb://localhost/pass', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+require('./auth/auth')
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Routes
+const routes = require('./routes/routes');
+const secureRoute = require('./routes/secure-routes');
+
+app.use('/', routes);
+//We plugin our jwt strategy as a middleware so only verified users can access this route
+app.use('/user', passport.authenticate('jwt', { session : false }), secureRoute );
+
+//Handle errors
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error : err });
+});
+
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// default value for title local
+app.locals.title = 'Sign up , Login and use CRUD!!';
+
+
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+module.exports = app;
